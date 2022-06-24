c780730122cf6843057f89154179d6d4a40ce101
Rodrigo <rodrigo.persoglia@gmail.com>
2022-06-24 16:53:13 -0300

    TP finalizado

--- LabNet2022.TP8.APIREST/Controllers/HomeController.cs
+++ LabNet2022.TP8.APIREST/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using LabNet2022.TP7.Logic.Services;
+using System;
+using System.Web.Mvc;
 
 namespace LabNet2022.TP8.APIREST.Controllers
 {
@@ -10,5 +12,20 @@ namespace LabNet2022.TP8.APIREST.Controllers
 
             return View();
         }
+
+
+        public ActionResult Provincias()
+        {
+            try
+            {
+                var _service = new ServiceProvincias();
+                var model = _service.GetProvincias();
+                return View(model);
+            }
+            catch (Exception) { return RedirectToAction("Index"); }
+        }
+
     }
+
+
 }
